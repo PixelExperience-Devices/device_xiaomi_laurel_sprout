18907964646c6468b073a7d5f8221027de737a71
Jun Wang <junwan@codeaurora.org>
2019-05-16 11:27:45 +0800

    sm6125-common: Add atrace-hal build rule
    CRs-Fixed: 2453668
    Change-Id: If995d1bf5cae68e09c156ac297fd7a5f5ca0d209

--- common.mk
+++ common.mk
@@ -78,6 +78,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# ATRACE_HAL
+PRODUCT_PACKAGES += \
+    android.hardware.atrace@1.0-service
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
