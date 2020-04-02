1b9123f6eb5aa7a3bfc35908f540b567f39db072
Henrique Pereira <hlcpereira@pixelexperience.org>
2020-04-02 09:12:08 +0000

    laurel_sprout: Enable TARGET_INCLUDE_WIFI_EXT

--- aosp_laurel_sprout.mk
+++ aosp_laurel_sprout.mk
@@ -23,6 +23,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 720
 TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_WIFI_EXT := true
 
 PRODUCT_SHIPPING_API_LEVEL := 28
 
