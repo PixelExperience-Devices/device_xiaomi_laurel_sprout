f1546db6431d6bcfff6db8758fdcf9c29a7a50d1
Kuba Wojciechowski <nullbytepl@gmail.com>
2020-04-27 17:26:14 +0000

    laurel_sprout: Ship ARCore

--- aosp_laurel_sprout.mk
+++ aosp_laurel_sprout.mk
@@ -24,6 +24,7 @@ $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 720
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_WIFI_EXT := true
+TARGET_INCLUDE_STOCK_ARCORE := true
 
 PRODUCT_SHIPPING_API_LEVEL := 28
 
