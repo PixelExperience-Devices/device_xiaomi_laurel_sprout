5e139b00a7f32b3c0af6b9d17cbe822aed1a4d4f
HarukeyUA <inazarrusnak@gmail.com>
2020-05-14 11:22:34 +0300

    laurel_sprout: Don't build livedisplay-sysfs service

--- device.mk
+++ device.mk
@@ -314,8 +314,7 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm \
-    vendor.lineage.livedisplay@2.0-service-sysfs
+    vendor.lineage.livedisplay@2.0-service-sdm
 
 # Media
 PRODUCT_COPY_FILES += \
