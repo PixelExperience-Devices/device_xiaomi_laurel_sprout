0423cfb3f0ef438e1f2af0a038e47563f7557cb3
HarukeyUA <inazarrusnak@gmail.com>
2020-05-19 18:33:34 +0300

    Revert "Revert "violet: Switch to more generic dt2w node""
    This reverts commit f903110aabe0d1de701a38fa312ca2889e92deaa.

--- power/service.cpp
+++ power/service.cpp
@@ -27,7 +27,7 @@
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#define LOG_TAG "android.hardware.power@1.2-service.ginkgo"
+#define LOG_TAG "android.hardware.power@1.2-service.laurel_sprout"
 
 #include <android/log.h>
 #include <hidl/HidlTransportSupport.h>
