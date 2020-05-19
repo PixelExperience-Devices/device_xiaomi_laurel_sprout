765a61a065f419462162db6614cb686820a241f4
HarukeyUA <inazarrusnak@gmail.com>
2020-05-19 18:16:36 +0300

    laurel_sprout: Add prebuild sensors 2.0 hal

--- device.mk
+++ device.mk
@@ -425,8 +425,6 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service \
     libsensorndkbridge
 
 # Soong namespaces
