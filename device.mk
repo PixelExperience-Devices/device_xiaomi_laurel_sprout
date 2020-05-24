5632c0a3371910d957459a2e3a8d62fe833d85bf
HarukeyUA <inazarrusnak@gmail.com>
2020-05-24 12:52:30 +0300

    laurel_sprout: Drop preopt for systemui and launcher
    * LineageOS doesn't ship pixel launcher
    * Preopt for systemui enabled by default

--- device.mk
+++ device.mk
@@ -410,11 +410,6 @@ PRODUCT_PACKAGES += \
     libcodec2_vndk.vendor \
     libcodec2_hidl@1.0.vendor
 
-# AOT Preload
-PRODUCT_DEXPREOPT_SPEED_APPS += \
-    SystemUI \
-    NexusLauncherRelease
-
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/codec2.vendor.ext.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/codec2.vendor.ext.policy \
