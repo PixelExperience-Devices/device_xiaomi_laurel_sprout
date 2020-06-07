57cca08e6dc5d52586d04372ca2cbbd1a9b149fd
Kuba Wojciechowski <nullbytepl@gmail.com>
2020-06-07 19:46:46 +0000

    laurel_sprout: use coral fingerprint
    Signed-off-by: Kuba Wojciechowski <nullbytepl@gmail.com>

--- aosp_laurel_sprout.mk
+++ aosp_laurel_sprout.mk
@@ -27,7 +27,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi A3
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "Xiaomi/laurel_sprout/laurel_sprout:10/QKQ1.190910.002/V11.0.15.0.QFQMIXM:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:10/QQ3A.200605.001/6392402:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="laurel_sprout-user 10 QKQ1.190910.002 V11.0.15.0.QFQMIXM release-keys" \
