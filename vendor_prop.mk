a2019a9203917552e6ee0222a88a06e1d96634da
Kuba Wojciechowski <nullbytepl@gmail.com>
2020-06-14 13:47:58 +0200

    laurel_sprout: force usage of QRD acdb
    * Indian/global models of Mi A3 (M1906F9SI) use QRD_India acdb by default (xiaomi modified their acdb-loader to read model code from kernel and load corresponding acdb). We don't ship those blobs, so just force the use of regular QRD blobs, as they work just fine
    
    Signed-off-by: Kuba Wojciechowski <nullbytepl@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -7,6 +7,17 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.sys.fw.dex2oat_thread_count=8 \
     ro.vendor.qti.am.reschedule_service=true
 
+# ACDB override
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.audio.calfile0=/vendor/etc/acdbdata/QRD/QRD_Bluetooth_cal.acdb \
+    persist.vendor.audio.calfile1=/vendor/etc/acdbdata/QRD/QRD_Codec_cal.acdb \
+    persist.vendor.audio.calfile2=/vendor/etc/acdbdata/QRD/QRD_General_cal.acdb \
+    persist.vendor.audio.calfile3=/vendor/etc/acdbdata/QRD/QRD_Global_cal.acdb \
+    persist.vendor.audio.calfile4=/vendor/etc/acdbdata/QRD/QRD_Handset_cal.acdb \
+    persist.vendor.audio.calfile5=/vendor/etc/acdbdata/QRD/QRD_Hdmi_cal.acdb \
+    persist.vendor.audio.calfile6=/vendor/etc/acdbdata/QRD/QRD_Headset_cal.acdb \
+    persist.vendor.audio.calfile7=/vendor/etc/acdbdata/QRD/QRD_Speaker_cal.acdb
+
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
     af.fast_track_multiplier=2 \
