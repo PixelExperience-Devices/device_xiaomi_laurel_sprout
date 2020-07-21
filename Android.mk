f3f0e88988584dbc02535cde92955f175ce8d68f
Erfan Abdi <erfangplus@gmail.com>
2020-07-21 22:53:16 +0430

    sm6125-common: Setup filter for ginkgo and laurel
    Change-Id: I4d6af75d6a94855cdd2dd35ed80754b4bf7544c0

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter jasmine_sprout jason lavender twolip wayne whyred,$(TARGET_DEVICE)),)
+ifneq ($(filter laurel_sprout ginkgo,$(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
 
