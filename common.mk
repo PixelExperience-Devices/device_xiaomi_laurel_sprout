e04e7c971420add1df4673f5592090ea2399a5c3
Erfan Abdi <erfangplus@gmail.com>
2020-07-23 00:38:11 +0430

    sm6125-common: Nuke android.hidl.manager@1.0_system
    Change-Id: I60eeb3e1f4850c9fa1359756576ddb657ea20d99

--- common.mk
+++ common.mk
@@ -258,8 +258,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0_system \
-    android.hidl.manager@1.0 \
-    android.hidl.manager@1.0_system
+    android.hidl.manager@1.0
 
 # Init
 PRODUCT_PACKAGES += \
