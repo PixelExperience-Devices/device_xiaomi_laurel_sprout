53e4bd5ea6584e67af2e15b7e7e5a819a9aaa9d6
Erfan Abdi <erfangplus@gmail.com>
2020-07-22 22:16:12 +0430

    sm6125-common: Drop kernel additional flags
    Change-Id: Ie8852fb585b36af0bb1d0a68cd3dec142ef6e6e9

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -62,9 +62,6 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6125
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_ADDITIONAL_FLAGS := \
-    DTC=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/dtc/dtc \
-    MKDTIMG=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/libufdt/mkdtimg
 
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
