2196c3e0a51a45befb939ede6d7209e5376d805e
Erfan Abdi <erfangplus@gmail.com>
2020-07-24 04:00:37 +0430

    sm6125-common: Set max acquired frame buffers prop
    Change-Id: I962906f63ce9285f3e57fef3522efac68dcf77ea

--- properties.mk
+++ properties.mk
@@ -112,6 +112,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.latch_unsignaled=1 \
     ro.opengles.version=196610 \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.max_virtual_display_dimension=4096 \
     ro.surface_flinger.protected_contents=true \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
