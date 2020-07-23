23bd11d59e887232d0911fe2be228760c720e518
Erfan Abdi <erfangplus@gmail.com>
2020-07-24 04:04:53 +0430

    sm6125-common: Add powerhint.xml
    Change-Id: Ib3b8fcb4c55e39708b3c505f06e4b36a15b7852f

--- common.mk
+++ common.mk
@@ -354,6 +354,7 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.2-service-qti
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
     $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
 
 # Public Libraries
