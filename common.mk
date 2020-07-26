40492fb07db1ba79d6a4d0bc7dd650ca3cf251d0
Erfan Abdi <erfangplus@gmail.com>
2020-07-26 11:45:44 +0430

    sm6125-common: Down ver soundtrigger to 2.1
    This reverts commit 406d3a31159a2a3dfe2f466168c4090130ed740f.
    
    * Ginkgo blob only works with 2.1
    
    Change-Id: Ic6fe88d33bc9daaa7ef68252d27a913dc8d20962

--- common.mk
+++ common.mk
@@ -88,7 +88,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.audio.effect@2.0-service \
-    android.hardware.soundtrigger@2.2-impl \
+    android.hardware.soundtrigger@2.1-impl \
     audio.a2dp.default \
     audio.primary.trinket \
     audio.r_submix.default \
