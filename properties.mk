74150ba73586e29d73115c156ca8adfd270c43c8
Pig <pig.priv@gmail.com>
2020-11-04 04:45:01 +0800

    sm6125-common: Enable new file encryption flag
     * Enforced as of tag LA.UM.9.1.r1-07300-SMxxx0.0
     (Original commit: https://source.codeaurora.org/quic/la/platform/vendor/qcom/steppe/commit/?h=LA.UM.9.1.r1-07300-SMxxx0.0&id=910a57f09c7dc34dad01e2f4c4f2d6c02df85df2)
    
     * Properties taken from: https://source.android.com/security/encryption/metadata#configuration-on-adoptable-storage.
    
    Change-Id: I1621c537474a62fa851b234258d9da19fa523ad4

--- properties.mk
+++ properties.mk
@@ -116,8 +116,10 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 
 # Crypto
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.crypto.allow_encrypt_override=true \
-    ro.crypto.volume.filenames_mode=aes-256-cts
+    ro.crypto.dm_default_key.options_format.version=2 \
+    ro.crypto.volume.filenames_mode=aes-256-cts \
+    ro.crypto.volume.metadata.method=dm-default-key \
+    ro.crypto.volume.options=::v2
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
