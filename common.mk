186943557d053834518592a3045303c2bf81cdcd
Alexander Koskovich <zvnexus@outlook.com>
2020-11-18 15:10:31 -0700

    sm6125-common: Use the new AIDL QTI thermal HAL
    Change-Id: Ib9d983ad3eea58d840d2c5f8b42d5c6845b69906

--- common.mk
+++ common.mk
@@ -389,6 +389,10 @@ PRODUCT_PACKAGES += \
     libipanat \
     liboffloadhal
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.qti
+
 # Trust
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
