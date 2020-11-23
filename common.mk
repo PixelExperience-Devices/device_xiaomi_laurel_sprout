a176b0ffbd182744e2737fdb2cf3c0cf30b3d736
Michael Bestas <mkbestas@lineageos.org>
2020-11-23 20:47:24 +0200

    sm6125-common: Don't build textclassifier
    * Target no longer exists
    
    Change-Id: I2079d7dbbeb59bbe9cd925a56f6bcc84c8a38d7f

--- common.mk
+++ common.mk
@@ -413,10 +413,6 @@ PRODUCT_PACKAGES += \
     libipanat \
     liboffloadhal
 
-# TextClassifier smart selection model files
-PRODUCT_PACKAGES += \
-    textclassifier.bundle1
-
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
