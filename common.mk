d22c9abebf98ee3cbb8f835f514ed8c1ae898261
dianlujitao <dianlujitao@lineageos.org>
2021-02-09 18:14:04 +0800

    sm6125-common: Switch to source built vendor.qti.hardware.perf@2.2.so
    Change-Id: Ia5de8c300d71b6def223e97ef9d58898c9c11a57

--- common.mk
+++ common.mk
@@ -326,7 +326,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service-qti \
+    vendor.qti.hardware.perf@2.2.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
