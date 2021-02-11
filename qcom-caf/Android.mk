fd102ef98200df784a61f68172e7c584bf511d82
Nazar Rusnak <inazarrusnak@gmail.com>
2021-02-11 17:18:32 +0200

    sm6125-common: Drop in-tree display hal for now

--- qcom-caf/Android.mk
+++
@@ -1,7 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-HAL_PLATFORM := sm8150
-HAL_PATH := hardware/qcom-caf/$(HAL_PLATFORM)
-
-include $(call all-makefiles-under,$(LOCAL_PATH))
-include $(call all-makefiles-under,$(HAL_PATH)/audio)
-include $(call all-makefiles-under,$(HAL_PATH)/media)
