69d8406172a93171d779af9022fb12cffb130fc7
Nazar Rusnak <inazarrusnak@gmail.com>
2021-02-21 17:34:02 +0200

    sm6125-common: Update libavservices_minijail name

--- common.mk
+++ common.mk
@@ -307,7 +307,7 @@ PRODUCT_COPY_FILES += \
 
 # OMX
 PRODUCT_PACKAGES += \
-    libavservices_minijail_vendor \
+    libavservices_minijail.vendor \
     libc2dcolorconvert \
     libhypv_intercept \
     libmm-omxcore \
