5292cecc28c6885df1a103a5da538c1795b9c5a1
Nazar Rusnak <inazarrusnak@gmail.com>
2021-02-21 19:56:25 +0200

    sm6125-common: Build additional mapper interfaces

--- common.mk
+++ common.mk
@@ -197,6 +197,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.composer@1.0.vendor \
     vendor.qti.hardware.display.composer@2.0.vendor \
+    vendor.qti.hardware.display.mapper@1.0.vendor \
+    vendor.qti.hardware.display.mapper@1.1.vendor \
+    vendor.qti.hardware.display.mapper@2.0.vendor \
     vendor.qti.hardware.display.mapper@4.0.vendor
 
 # DRM
