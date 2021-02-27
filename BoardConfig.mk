57ec2c94a04d9a59f499d08d4cf3b64fd3755725
HarukeyUA <inazarrusnak@gmail.com>
2021-02-27 10:14:04 +0200

    laurel_sprout: Update from V12.0.6.0.RFQMIXM

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -67,7 +67,7 @@ TARGET_NO_RECOVERY := true
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2021-01-05
+VENDOR_SECURITY_PATCH := 2021-02-05
 
 # SELinux
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
