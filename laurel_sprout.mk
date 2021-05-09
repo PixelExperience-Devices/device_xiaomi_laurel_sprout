ceb69c751ce013b13e6f95860af96c623c10ff67
Mrinal Ghosh <mg712702@gmail.com>
2021-05-09 11:20:30 +0530

    laurel_sprout: Switch back to android.hardware.thermal@1.0 HAL
    * Fixes: "Device is too hot" issue in some apps like
      Google Photos and don't let the users backup photos.
    
    This reverts commit 186943557d053834518592a3045303c2bf81cdcd.
    
    Change-Id: I432bc178b58ce2a78ce0d42bdb63381588ef951b
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- laurel_sprout.mk
+++ laurel_sprout.mk
@@ -444,7 +444,9 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.qti
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service \
+    thermal.trinket
 
 # Trust
 PRODUCT_PACKAGES += \
