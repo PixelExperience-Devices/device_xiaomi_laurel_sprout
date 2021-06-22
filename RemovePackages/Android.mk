93c248ef6a388ea5cf1c21c0f91b9ebaeee98773
Lunarixus <Nathanb644@hotmail.co.uk>
2021-06-22 12:18:04 +0530

    laurel_sprout: Remove packages at build time
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

---
+++ RemovePackages/Android.mk
@@ -0,0 +1,15 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := RemovePackages
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt Camera2 CarrierSetup
+LOCAL_OVERRIDES_PACKAGES += MyVerizonServices Music
+LOCAL_OVERRIDES_PACKAGES += OBDM_Permissions OPScreenRecord RecorderPrebuilt
+LOCAL_OVERRIDES_PACKAGES += SafetyHubPrebuilt SprintDM SprintHM
+LOCAL_OVERRIDES_PACKAGES += VzwOmaTrigger YouTube YouTubeMusicPrebuilt
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
--- laurel_sprout.mk
+++ laurel_sprout.mk
@@ -382,6 +382,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fstab.qcom
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # RenderScript HAL
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
