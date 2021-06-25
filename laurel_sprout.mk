a257ce5baf27414b887eb69f160b59352923af80
Hemant Sachdeva <hemant.evolver@gmail.com>
2021-06-25 19:28:25 +0530

    laurel_sprout: Kang Hotword Enrollment blobs from Apollo
    * Fixes 'Ok Google' and 'Hey Google' voice recognition
    
    [kondors1995] and [bgcngm]: Update Hotword permissions
    
    Co-Authored-by: Bruno Martins <bgcngm@gmail.com>
    Co-Authored-by: Ronalds Ustinovs <kondors1995@users.noreply.github.com>
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- laurel_sprout.mk
+++ laurel_sprout.mk
@@ -255,6 +255,12 @@ PRODUCT_PACKAGES += \
     libhidltransport.vendor \
     libhwbinder.vendor
 
+# Hotword Permissions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/hotwords/com.android.hotwordenrollment.common.util.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/com.android.hotwordenrollment.common.util.xml \
+    $(LOCAL_PATH)/configs/hotwords/hotword-whitelist.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/hotword-whitelist.xml \
+    $(LOCAL_PATH)/configs/hotwords/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
+
 # Init
 PRODUCT_PACKAGES += \
     init.class_main.sh \
