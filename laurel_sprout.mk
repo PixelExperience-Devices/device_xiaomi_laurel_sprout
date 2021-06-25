b573db466ecb03dc995d1b04423eaab9547401b1
Hemant Sachdeva <hemant.evolver@gmail.com>
2021-06-25 19:57:14 +0530

    laurel_sprout: Drop SoundTrigger HAL
    * Hotwords not working in the presence of this HAL
    
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- laurel_sprout.mk
+++ laurel_sprout.mk
@@ -43,7 +43,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
-    android.hardware.soundtrigger@2.1-impl \
     audio.a2dp.default \
     audio.primary.trinket \
     audio.r_submix.default \
