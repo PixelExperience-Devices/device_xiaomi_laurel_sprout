0a52b5578ff3cd0af5232cae4db13047758d75db
Bruno Martins <bgcngm@gmail.com>
2021-10-21 22:57:54 +0530

    laurel_sprout: Fix A2DP when offload is disabled
    Ensure that A2DP still works in case one disables offload in the
    Developer options for whatever reason.
    
    Change-Id: I89fd37962b2d063d6e30f84234568d552131fd97
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- laurel_sprout.mk
+++ laurel_sprout.mk
@@ -68,6 +68,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/configs/audio/listen_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/listen_platform_info.xml \
     $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/configs/audio/audio_policy_configuration_a2dp_offload_disabled.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration_a2dp_offload_disabled.xml \
     $(LOCAL_PATH)/configs/audio/audio_tuning_mixer_tavil.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer_tavil.txt \
     $(LOCAL_PATH)/configs/audio/graphite_ipc_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/graphite_ipc_platform_info.xml \
     $(LOCAL_PATH)/configs/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_intcodec.xml \
