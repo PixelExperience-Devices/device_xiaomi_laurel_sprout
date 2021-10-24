48a8b6aa027a9f6275b0213801ee0a1a12e2ba63
Magicxavi <rolfpeterkesche@gmail.com>
2021-10-24 13:29:07 +0530

    laurel_sprout: Build android.hardware.radio and configs packages for RIL
    F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/qcrild": library "android.hardware.radio.config@1.0.so" not found
    
    F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/qcrild": library "android.hardware.radio@1.2.so" not found
    
    F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/qcrild": library "android.hardware.radio.deprecated@1.0.so" not found: needed by /vendor/lib64/libril-qc-hal-qmi.so in namespace (default)
    
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- laurel_sprout.mk
+++ laurel_sprout.mk
@@ -425,7 +425,15 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
+    android.hardware.radio.config@1.0.vendor \
+    android.hardware.radio.config@1.1.vendor \
+    android.hardware.radio.config@1.2.vendor \
     android.hardware.secure_element@1.0 \
+    android.hardware.radio@1.2.vendor \
+    android.hardware.radio@1.3.vendor \
+    android.hardware.radio@1.4.vendor \
+    android.hardware.radio@1.5.vendor \
+    android.hardware.radio.deprecated@1.0.vendor \
     rild \
     librmnetctl \
     libxml2 \
