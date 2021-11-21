e22ea3727088e1366fef5f2e3d468af47e9f79a2
Magicxavi <rolfpeterkesche@gmail.com>
2021-11-21 02:25:10 +0200

    laurel_sprout: Import powerhint.json from crosshatch
    Ref: https://android.googlesource.com/device/google/crosshatch/+/refs/tags/android-12.0.0_r15/powerhint.json
    
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- laurel_sprout.mk
+++ laurel_sprout.mk
@@ -382,7 +382,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.perf@2.2.vendor
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/perf/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
+    $(LOCAL_PATH)/configs/perf/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json \
     $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
 
 # Public Libraries
