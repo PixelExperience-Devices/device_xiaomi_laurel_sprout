fff925e5c33b148b66bef2a2feabb80f2289a984
Magicxavi <rolfpeterkesche@gmail.com>
2021-11-24 13:21:47 +0200

    laurel_sprout: power-libperfmgr: Adapt power service for laurel
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- power-libperfmgr/hidl/service.cpp
+++ power-libperfmgr/hidl/service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.power@1.3-service.pixel-libperfmgr"
+#define LOG_TAG "android.hardware.power@1.3-service.laurel_sprout-libperfmgr"
 
 #include <android/log.h>
 #include <hidl/HidlTransportSupport.h>
