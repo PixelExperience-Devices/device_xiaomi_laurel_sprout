e27535603778ef41bce9fd1e09c2d4c94847c8fb
Hemant Sachdeva <hemant.evolver@gmail.com>
2021-12-01 18:24:53 +0530

    laurel_sprout: RemovePackages: Do not ship Google Drive
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt Camera2 CarrierSetup
+LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt Camera2 CarrierSetup Drive
 LOCAL_OVERRIDES_PACKAGES += Maps MyVerizonServices Music
 LOCAL_OVERRIDES_PACKAGES += OBDM_Permissions OPScreenRecord RecorderPrebuilt
 LOCAL_OVERRIDES_PACKAGES += SafetyHubPrebuilt SprintDM SprintHM
