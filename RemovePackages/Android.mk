651c7ad492ad3d7309395e67cebfa2139a933134
Hemant Sachdeva <hemant.evolver@gmail.com>
2022-04-01 20:37:46 +0530

    laurel_sprout: Remove more packages
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -4,13 +4,40 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt AppDirectedSMSService arcore Camera2 CarrierSetup
-LOCAL_OVERRIDES_PACKAGES += DevicePolicyPrebuilt DiagnosticsToolPrebuilt DMService Drive GoogleCamera
-LOCAL_OVERRIDES_PACKAGES += GoogleFeedback Maps Music MyVerizonServices NgaResources
-LOCAL_OVERRIDES_PACKAGES += OBDM_Permissions obdm_stub OemDmTrigger OPScreenRecord Photos
-LOCAL_OVERRIDES_PACKAGES += PrebuiltGmail PixelLiveWallpaperPrebuilt RecorderPrebuilt
-LOCAL_OVERRIDES_PACKAGES += SafetyHubPrebuilt SprintDM SprintHM Tycho USCCDM
-LOCAL_OVERRIDES_PACKAGES += VZWAPNLib VzwOmaTrigger WallpapersBReel2020 YouTube YouTubeMusicPrebuilt
+LOCAL_OVERRIDES_PACKAGES := \
+	AmbientSensePrebuilt \
+	AppDirectedSMSService \
+	CalendarGooglePrebuilt \
+	Camera2 \
+	CarrierSetup \
+	ConnMO \
+	DCMO \
+	DMService \
+	DevicePolicyPrebuilt \
+	DiagnosticsToolPrebuilt \
+	Drive \
+	HelpRtcPrebuilt \
+	Maps \
+	MyVerizonServices \
+	NgaResources \
+	OBDM_Permissions \
+	OemDmTrigger \
+	Photos \
+	PixelLiveWallpaperPrebuilt \
+	PixelWallpapers2021 \
+	PrebuiltGmail \
+	RecorderPrebuilt \
+	SafetyHubPrebuilt
+	ScribePrebuilt \
+	Showcase \
+	SoundAmplifierPrebuilt \
+	SprintDM \
+	SprintHM
+	Tycho \
+	USCCDM \
+	VZWAPNLib \
+	VzwOmaTriger
+
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
