3588ae788a4adf1b0d4e3b55c50ba07bec7a4f34
zlewchan <wiktorusyt@protonmail.ch>
2022-05-07 02:08:21 +0200

    laurel_sprot: Resolve neverallows
    Signed-off-by: zlewchan <wiktorusyt@protonmail.ch>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -172,7 +172,7 @@ VENDOR_SECURITY_PATCH := 2021-05-05
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
-SELINUX_IGNORE_NEVERALLOWS := true
+# SELINUX_IGNORE_NEVERALLOWS := true
 
 # Treble
 BOARD_VNDK_VERSION := current
